1eda61e89898684713a3e483ab52947f617e24f3
Baranseli <baranselias@hotmail.com>
2019-05-11 17:52:49 +0300

    Initial Commit

---
+++ Smack Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack Chat
+//
+//  Created by Kafkas Baranseli on 11/05/2019.
+//  Copyright © 2019 Baranseli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
